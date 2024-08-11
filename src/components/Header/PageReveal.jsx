a0bdd9dd3015379fc2861ea238909ef100589a03
Bancantcode <santiagobryan831@gmail.com>
2024-08-11 13:45:51 +0800

    initial commit

---
+++ src/components/Header/PageReveal.jsx
@@ -0,0 +1,50 @@
+'use client'
+import { useEffect } from 'react';
+import './styles.scss'
+import gsap from 'gsap';
+
+export default function PageReveal() {
+  
+  useEffect(() => {
+    gsap.set(".img", { y: 550 })
+    gsap.set(".loader_imgs", { x: 500 })
+
+    const tl = gsap.timeline({ delay: 0.5 })
+    tl
+    .to(".img", { y: 0, duration: 1, stagger: 0.05, ease: "power3.inOut", })
+    .to(".loader_imgs", { x: 0, duration: 3, ease: "power3.inOut" }, "-=2.5")
+    .to(".img:not(#loader-logo)", { clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)", duration: 1, stagger: 0.1, ease: "power3.inOut", }, "-=1")
+    .to(".loader", { clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)", duration: 1, ease: "power3.inOut", }, "-=0.5")
+  })
+
+  return (
+     <div className="loader">
+      <div className="loader_imgs">
+        <div className="img">
+          <img className="image" src="images/about_filter.png" alt="" />
+        </div>
+        <div className="img">
+          <img className="image" src="images/about_image1.png" alt="" />
+        </div>
+        <div className="img">
+          <img className="image" src="images/about_image4.png" alt="" />
+        </div>
+        <div className="img" id="loader-logo">
+          <img className="image" src="images/about_filter.png" alt="" />
+        </div>
+        <div className="img">
+          <img className="image" src="images/about_milk.png" alt="" />
+        </div>
+        <div className="img">
+          <img className="image" src="images/home_image3.png" alt="" />
+        </div>
+        <div className="img">
+          <img className="image" src="images/main_moreInformation1.png" alt="" />
+        </div>
+      </div>
+     </div>
+
+     /* website content here */
+
+  );
+}
