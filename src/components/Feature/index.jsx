7076439a4b800dde6b2e33107eca2e05c67a7a13
Bancantcode <santiagobryan831@gmail.com>
2024-08-23 09:34:24 +0800

    scrolltrigger added for images in tablet and mobile

--- src/components/Feature/index.jsx
+++ src/components/Feature/index.jsx
@@ -6,16 +6,45 @@ import Feature2 from '../../../public/images/feature2.png';
 import Feature3 from '../../../public/images/feature3.png';
 import Feature4 from '../../../public/images/feature4.png';
 import Feature5 from '../../../public/images/feature5.png';
+import { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Feature() {
     const feature__images = [Feature4, Feature2, Feature1, Feature3, Feature5];
+    const container = useRef(null);
+    const imagesRef = useRef([])
+
+    useLayoutEffect(() => {
+        const context = gsap.context(() => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: container.current,
+                    start: "top bottom",
+                    end: "bottom top",
+                    scrub: true,
+                    // markers: true,
+                }
+            });
+    
+            tl.to(imagesRef.current[0], { x: -20, y: 10 }, 0);
+            tl.to(imagesRef.current[1], { x: -25, y: -20 }, 0);
+            tl.to(imagesRef.current[2], { y: -30 }, 0);
+            tl.to(imagesRef.current[3], { x: 20, y: -10 }, 0);
+            tl.to(imagesRef.current[4], { x: 10, y: 20 }, 0);
+        });
+    
+        return () => context.revert();
+    }, []);
 
     return (
-        <div className={styles.container}>
+        <div ref={container} className={styles.container}>
             <div className={styles.images}>
                 {
                     feature__images.map((image, i) => {
-                        return <div key={`l_${i}`} className={styles.image__container}>
+                        return <div ref={ref => imagesRef.current[i] = ref} key={`l_${i}`} className={styles.image__container}>
                             <Image className={styles.img} src={image} placeholder='blur' alt="image" />
                         </div>
                     })
