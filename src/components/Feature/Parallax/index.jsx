c8e181bbb4d257153bc232d00eab0ebf575bea4e
Bancantcode <santiagobryan831@gmail.com>
2024-09-01 17:46:41 +0800

    framer motion onscroll

--- src/components/Feature/Parallax/index.jsx
+++ src/components/Feature/Parallax/index.jsx
@@ -1,15 +1,28 @@
+'use client';
+import { useEffect, useRef } from 'react';
+import { useScroll, motion } from 'framer-motion';
 import styles from './style.module.scss';
 
 function Parallax() {
+  const element = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ['start 0.9', 'start start'], //start of container, end of window and start of target, start of the window
+  })
+
+  // useEffect(() => {
+  //   scrollYProgress.on("change", e => console.log(e))
+  // })
+
   return (
     <div className={styles.container}>
-      <div className={styles.parallax}>
+      <motion.div ref={element} className={styles.parallax} style={{ opacity: scrollYProgress }}>
         <span>A coffee space designed</span>
         <span>to create, connect</span>
         <span>and celebrate</span>
         <span>the love of coffee</span>
         <span>together in every cup.</span>
-      </div>
+      </motion.div>
     </div>
   )
 }
