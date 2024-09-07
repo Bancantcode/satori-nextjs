5226fede0ba1a4735cb10b5888ad9ad7cbab0b84
Bancantcode <santiagobryan831@gmail.com>
2024-09-08 03:51:45 +0800

    uncomplete animation

--- src/components/Home/Team/index.jsx
+++ src/components/Home/Team/index.jsx
@@ -7,40 +7,47 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function MeetTheTeam() {
     const container = useRef(null);
-    const title1Ref = useRef(null);
-    const title2Ref = useRef(null);
+    const titleRef = useRef(null);
     const descriptionRef = useRef(null);
     const buttonRef = useRef(null);
 
     useLayoutEffect(() => {
-        gsap.set([title1Ref.current, title2Ref.current, descriptionRef.current, buttonRef.current], { y: 200, opacity: 0, clipPath: "polygon(0% 50%, 100% 50%, 100% 50%, 0% 50%)" });
+        const titleWords = titleRef.current.querySelectorAll("span");
+
+        gsap.set(titleWords, { y: 150, clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)" });
+        gsap.set(descriptionRef.current, { y: 30, clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)", });
+        gsap.set(buttonRef.current, { y: 80, clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)", });
+
         const context = gsap.context(() => {
             const tl = gsap.timeline({
                 scrollTrigger: {
                     trigger: container.current,
-                    start: 'top bottom',
-                    end: 'bottom top',
+                    start: "top bottom",
+                    end: "-=300",
                     scrub: true,
                 },
-            })
-            
-            tl.to(title1Ref.current, { y: 0, opacity: 1, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", duration: 0.5 })
-              .to(title2Ref.current, { y: 0, opacity: 1, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", duration: 0.5 })
-              .to(descriptionRef.current, { y: 0, opacity: 1, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", duration: 0.5 })
-              .to(buttonRef.current, { y: 0, opacity: 1, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", duration: 0.5 })
-        })
+            });
+
+            tl.to(titleWords, { y: 0, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", stagger: 0.1, duration: 1, ease: "power3.inOut", })
+              .to(descriptionRef.current, { y: 0, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", duration: 0.7, ease: "power3.inOut", },"-=0.4" )
+              .to(buttonRef.current, { y: 0, clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", duration: 0.7, ease: "power3.inOut", }, "-=0.4" )
+        });
 
         return () => context.revert();
     }, []);
 
     return (
         <div ref={container} className={styles.container}>
-            <div className={styles.content}>
-                <span ref={title1Ref}>Designed to bring</span><br />
-                <span ref={title2Ref}>a diverse experience</span>
+            <div ref={titleRef} className={styles.content}>
+                <span>Designed to bring</span><br />
+                <span>a diverse experience</span>
+            </div>
+            <div className={styles.description__container}>
                 <p ref={descriptionRef} className={styles.description}>MEET OUR TEAM OF DEDICATED COFFEE EXPERTS, BLENDING PASSION AND EXPERTISE TO EVERY CUP</p>
+            </div>
+            <div className={styles.button__container}>
                 <a ref={buttonRef} href="/feature"><button className={styles.feature__button}>Feature</button></a>
             </div>
         </div>
-    )
+    );
 }
