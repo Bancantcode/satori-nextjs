be133e9aa636046699a5a89d996367a2211c7681
Bancantcode <santiagobryan831@gmail.com>
2024-09-10 00:02:30 +0800

    updated

--- src/components/Home/About/index.jsx
+++ src/components/Home/About/index.jsx
@@ -29,7 +29,7 @@ export default function About() {
                     start: "top bottom",
                     end: "+=800 bottom",
                     scrub: true,
-                    markers: true,
+                    // markers: true,
                 }
             });
 
