6bd46a2b65f17752ea9b0ed9f7c1758cd6f11395
Bancantcode <santiagobryan831@gmail.com>
2024-09-10 23:08:36 +0800

    fixed accordion

--- src/components/Home/Faqs/index.jsx
+++ src/components/Home/Faqs/index.jsx
@@ -11,24 +11,24 @@ export default function FAQs() {
 
     const faqs = [
         {
-            question: "Where is Satori located?",
-            answer: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            question: "What makes Satori Specialty Coffee different from other coffee brands?",
+            answer: "What sets us apart is our relentless dedication to sourcing the highest-quality beans from renowned coffee-growing regions around the world. Our expert roasters carefully craft each batch to bring out the unique flavors and aromas that define our specialty coffee. Beyond just great coffee, we focus on creating memorable moments for our customers, whether it's through a perfectly brewed cup in our cafe or a personalized experience when you order online. We believe that every sip of Satori coffee should be an exceptional experience that goes beyond the ordinary."
         },
         {
-            question: "How do you source your coffee beans?",
-            answer: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            question: "Where do you source your coffee beans from?",
+            answer: "We take immense pride in the transparency of our sourcing process. Our coffee beans are meticulously selected from Ethiopia and Brazil, where sustainable farming practices and ethical standards are paramount. We prioritize direct relationships with farmers to ensure fair trade and quality control at every step. By sharing the origin of our beans, we invite you to connect with the story behind each cup, embracing a journey that begins at the source and culminates in the delightful experience you savor with every sip."
         },
         {
-            question: "Do you host any events or coffee tasting?",
-            answer: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            question: "How do you ensure the freshness of your coffee?",
+            answer: "Ensuring the freshness of our coffee is a top priority at Satori. We meticulously roast our coffee beans in small batches to maintain optimal flavor and aroma. Additionally, we package our coffee immediately after roasting, utilizing specialized techniques to preserve its freshness during storage and transportation. From bean to cup, we're committed to delivering an unparalleled coffee experience."
         },
         {
-            question: "How do you source your coffee beans?",
-            answer: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            question: "Do you offer any specialty drinks or unique flavor combinations?",
+            answer: "At Satori, we take pride in crafting a diverse menu that caters to all tastes. In addition to our exceptional espresso, filter, and milk-based coffees, we also offer a selection of specialty drinks featuring unique flavor combinations that are sure to tantalize your taste buds. Whether you're craving a classic espresso or looking to explore something new and exciting, we have the perfect beverage to accompany our delicious pastries. Our menu is designed to delight and surprise, ensuring that every visit to Satori is a memorable experience."
         },
         {
-            question: "How do you source your coffee beans?",
-            answer: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            question: "What sets your coffee shop ambiance apart?",
+            answer: "We believe ambiance is as important as the coffee itself. Our coffee shop offers a unique and inviting atmosphere that sets us apart. From the moment you step through our doors, you'll be enveloped in a warm and welcoming environment, designed to inspire relaxation and creativity. Our carefully curated decor, comfortable seating options, and soothing background music create the perfect backdrop for enjoying your favorite brew or pastry. Whether you're looking for a quiet space to work or a cozy spot to catch up with friends, our ambiance sets the stage for a truly memorable coffee experience."
         },
     ];
 
