feee13e64133a9dc36833d71b4f23bc4e29ff71e
Bancantcode <santiagobryan831@gmail.com>
2024-09-13 09:12:30 +0800

    mobile and tablet DOING component done

--- src/components/Feature/Doing/index.jsx
+++ src/components/Feature/Doing/index.jsx
@@ -4,39 +4,39 @@ function Doing() {
   return (
     <div className={styles.container}>
         <div className={styles.title}>
-            <span>What we</span>
+            <span>What we</span><br />
             <span>Love doing</span>
         </div>
         <div className={styles.outer__container}>
             <div className={styles.inner__container}>
-              <video  className={styles.video} src="/videos/Doing1.mp4" autoPlay loop muted controls={false} playsInline style={{ pointerEvents: 'none' }} />
               <div className={styles.flex__container}>
                 <div className={styles.number}>01</div>
                 <div className={styles.context}>
-                  <div className={styles.title}>Strategy</div>
-                  <p className={styles.description}>CRAFTING UNIQUE COFFEE EXPERIENCE, ENHANCING CUSTOMER CONNECTIONS, SHARING COFFEE STORIES, COLLABORATING WITH COFFEEINFLUENCES, SOCIAL MEDIA STRATEGY</p>
+                  <div className={styles.product__title}>Strategy</div>
+                  <p className={styles.description}>CRAFTING UNIQUE COFFEE,<br /> ENHANCING CUSTOMER CONNECTIONS,<br /> SHARING COFFEE STORIES,<br /> COLLABORATING WITH COFFEEINFLUENCES,<br /> SOCIAL MEDIA STRATEGY</p>
                 </div>
               </div>
+              <video className={styles.video} src="/videos/Doing2.mp4" autoPlay loop muted controls={false} playsInline style={{ pointerEvents: 'none' }} />
             </div>
             <div className={styles.inner__container}>
-              <video  className={styles.video} src="/videos/Doing2.mp4" autoPlay loop muted controls={false} playsInline style={{ pointerEvents: 'none' }} />
               <div className={styles.flex__container}>
                 <div className={styles.number}>02</div>
                 <div className={styles.context}>
-                  <div className={styles.title}>Creation</div>
-                  <p className={styles.description}>VISUAL CONCEPTS, ADVERTISING, BRANDING, COFFEE EVENTS, SCENOGRAPHY</p>
+                  <div className={styles.product__title}>Creation</div>
+                  <p className={styles.description}>VISUAL CONCEPTS,<br /> ADVERTISING,<br /> BRANDING,<br /> COFFEE EVENTS,<br /> SCENOGRAPHY</p>
                 </div>
               </div>
+              <video className={styles.video} src="/videos/Doing1.mp4" autoPlay loop muted controls={false} playsInline style={{ pointerEvents: 'none' }} />
             </div>
             <div className={styles.inner__container}>
-              <video  className={styles.video} src="/videos/Doing3.mp4" autoPlay loop muted controls={false} playsInline style={{ pointerEvents: 'none' }} />
               <div className={styles.flex__container}>
                 <div className={styles.number}>03</div>
                 <div className={styles.context}>
-                  <div className={styles.title}>Production</div>
-                  <p className={styles.description}>EVENTS, CONTENT, LIVE CONTENT</p>
+                  <div className={styles.product__title}>Production</div>
+                  <p className={styles.description}>EVENTS,<br /> CONTENT,<br /> LIVE CONTENT</p>
                 </div>
               </div>
+              <video className={styles.video} src="/videos/Doing3.mp4" autoPlay loop muted controls={false} playsInline style={{ pointerEvents: 'none' }} />
             </div>
         </div>
     </div>
