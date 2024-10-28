5c8bf72c45a128912df6d94ffba9113d6991d25e
Bancantcode <santiagobryan831@gmail.com>
2024-10-29 01:31:06 +0800

    fixed scrollbar

--- src/pages/email-mockup.js
+++ src/pages/email-mockup.js
@@ -2,8 +2,20 @@ import '../app/globals.css'
 import Header from '@/components/Header'
 import EmailMarketing from '@/components/Marketing/Email';
 import Footer from '@/components/Footer';
+import Lenis from 'lenis';
+import { useEffect } from 'react';
 
 export default function EmailMockup() {
+    useEffect(() => {
+        const lenis = new Lenis();
+        function raf(time) {
+          lenis.raf(time);
+          requestAnimationFrame(raf);
+        }
+        
+        requestAnimationFrame(raf);
+      }, [])
+
     return (
         <main>
             <Header />
--- src/pages/social-media-mockup.js
+++ src/pages/social-media-mockup.js
@@ -2,8 +2,20 @@ import '../app/globals.css';
 import Header from '@/components/Header'
 import SocialMediaMarketing from '@/components/Marketing/Social-media'
 import Footer from '@/components/Footer';
+import Lenis from 'lenis';
+import { useEffect } from 'react';
 
 export default function EmailMockup() {
+    useEffect(() => {
+        const lenis = new Lenis();
+        function raf(time) {
+          lenis.raf(time);
+          requestAnimationFrame(raf);
+        }
+        
+        requestAnimationFrame(raf);
+      }, [])
+
     return (
         <main>
             <Header />
