7402f41433de5c9a0b380c3a7110123929ce8b9f
Bancantcode <santiagobryan831@gmail.com>
2024-11-10 11:30:54 +0800

    adjusted header links

--- src/components/Header/nav/Footer/index.jsx
+++ src/components/Header/nav/Footer/index.jsx
@@ -3,10 +3,9 @@ import styles from './style.module.scss';
 export default function index() {
   return (
     <div className={styles.footer}>
-        <a>Facebook</a>
-        <a>Instagram</a>
-        <a>Github</a>
-        <a>LinkedIn</a>
+        <a href='https://www.facebook.com/satorispecialtycoffee' target='_blank'>Facebook</a>
+        <a href='https://www.instagram.com/satori.mnl/' target='_blank'>Instagram</a>
+        <a href='https://www.tiktok.com/@satori.mnl?is_from_webapp=1&sender_device=pc' target='_blank'>Tiktok</a>
     </div>
   )
 }
